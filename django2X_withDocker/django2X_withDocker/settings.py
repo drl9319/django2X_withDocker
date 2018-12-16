54e730afc6ed0d81eec3012d6340f8a2d8ad84aa
drl <drl.9319@gmail.com>
2018-12-16 20:33:24 +0100

    ADD new dependencies

--- django2X_withDocker/django2X_withDocker/settings.py
+++ django2X_withDocker/django2X_withDocker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4&e25!j%)^k+5p+f0$_1j-f^8qxv1&_w3+1wd*u(l3r_*8wm8f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
